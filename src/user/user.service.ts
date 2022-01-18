dcb07796c3e98d8d89e359078175e05b5635ca9f
user <user@EMERJ-DETEC2-NB.tj.rj.gov.br>
2022-01-18 13:26:57 -0300

    Deploy

---
+++ src/user/user.service.ts
@@ -0,0 +1,25 @@
+import { User as UserModel, Prisma } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+    constructor(private prisma: PrismaService) {}
+
+    async getUsers(): Promise<UserModel[]>{
+        return this.prisma.user.findMany({});
+    }
+    async getUserById(id: number): Promise<UserModel>{
+        return this.prisma.user.findFirst({
+            where: {id: +id},
+        })
+    }
+
+
+async createUser(data:Prisma.UserCreateInput): Promise<UserModel>{
+        return this.prisma.user.create({
+        data,
+        })
+}
+}
+
